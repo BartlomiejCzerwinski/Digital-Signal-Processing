618797c62253397e736be1cbe757a625586b0a47
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-03-16 15:20:13 +0100

    init generator.py

---
+++ Problem Set 1/generator.py
@@ -0,0 +1,53 @@
+
+A = 0
+t1 = 0
+d = 0
+T = 0
+kw = 0
+ts = 0
+p = 0
+f = 0
+
+#(S1) szum o rozkładzie jednostajnym;
+def uniformDistribution():
+    pass
+
+#(S2) szum gaussowski;
+def gaussianNoise():
+    pass
+
+#(S3) sygnał sinusoidalny;
+def sinusoidalSignal():
+    pass
+
+#(S4) sygnał sinusoidalny wyprostowany jednopołówkowo;
+def halfWaveRectifierSignal():
+    pass
+
+#(S5) sygnał sinusoidalny wyprostowany dwupołówkowo;
+def fullWaveRectifierSignal():
+    pass
+
+#(S6) sygnał prostokątny;
+def rectangularSignal():
+    pass
+
+#(S7) sygnał prostokątny symetryczny;
+def rectangularSymmetricalSignal():
+    pass
+
+#(S8) sygnał trójkątny;
+def triangularSignal():
+    pass
+
+#(S9) skok jednostkowy;
+def unitStep():
+    pass
+
+#(S10) impuls jednostkowy;
+def unitImpulse():
+    pass
+
+#(S11) szum impulsowy;
+def impulseNoise():
+    pass
