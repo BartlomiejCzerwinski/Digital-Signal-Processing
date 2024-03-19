3cfdda930d9b63ef9d4909cd44ab8566fa658a55
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-03-19 19:19:20 +0100

    updatwe

--- Problem Set 1/DSP/signal_and_noise_generation/views.py
+++ Problem Set 1/DSP/signal_and_noise_generation/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from . import forms
 
 def index(request):
+    if request.method == "POST":
+        fct = request.POST['function']
+        amplitude = request.POST['amplitude']
+        start_time = request.POST['start_time']
+        duration = request.POST['duration']
+        T = request.POST['T']
+        kw = request.POST['kw']
+        jump_time = request.POST['jump_time']
+        p = request.POST['p']
+        f = request.POST['f']
+
     form = forms.Form_add()
     return render(request, "signal_and_noise_generation/index.html",
                   {'form': form})
