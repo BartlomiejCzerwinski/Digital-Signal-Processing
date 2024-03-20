57f4024a652a3cfc46c429dbd473d59ff57b8d87
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-03-20 17:38:24 +0100

    update urls

--- Problem Set 1/DSP/signal_and_noise_generation/urls.py
+++ Problem Set 1/DSP/signal_and_noise_generation/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('draw_plot', views.draw_plot, name="draw_plot",)
+    path('draw_plot', views.draw_plot, name="draw_plot",),
+    path("operation", views.operation, name="operation"),
+    path("calculate_operation", views.calculate_operation, name="calculate_operation"),
 ]
