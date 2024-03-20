cd617198cc76d8edf6482540f080f1bb3444192b
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-03-20 19:00:36 +0100

    update views.py for operations

--- Problem Set 1/DSP/signal_and_noise_generation/views.py
+++ Problem Set 1/DSP/signal_and_noise_generation/views.py
@@ -2,22 +2,60 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from . import forms
 from . import generator
+from . import operator
 
 def index(request):
-    if request.method == "POST":
-        fct = request.POST['function']
-        amplitude = request.POST['amplitude']
-        start_time = request.POST['start_time']
-        duration = request.POST['duration']
-        T = request.POST['T']
-        kw = request.POST['kw']
-        jump_time = request.POST['jump_time']
-        p = request.POST['p']
-        f = request.POST['f']
-
     form = forms.Form_add()
     return render(request, "signal_and_noise_generation/index.html",
                   {'form': form})
+
+def operation(request):
+    form = forms.Form_operation()
+    return render(request, "signal_and_noise_generation/operation.html",
+                  {'form': form})
+
+def calculate_operation(request):
+    if request.method == 'POST':
+        print(request.POST)
+        fct1 = request.POST['function1']
+        amplitude1 = float(request.POST['amplitude1'])
+        start_time1 = float(request.POST['start_time1'])
+        duration1 = float(request.POST['duration1'])
+        T1 = float(request.POST['T1'])
+        kw1 = float(request.POST['kw1'])
+        jump_time1 = float(request.POST['jump_time1'])
+        p1 = float(request.POST['p1'])
+        f1 = int(request.POST['f1'])
+        operation = request.POST['operation']
+        fct2 = request.POST['function2']
+        amplitude2 = float(request.POST['amplitude2'])
+        start_time2 = float(request.POST['start_time2'])
+        duration2 = float(request.POST['duration2'])
+        T2 = float(request.POST['T2'])
+        kw2 = float(request.POST['kw2'])
+        jump_time2 = float(request.POST['jump_time2'])
+        p2 = float(request.POST['p2'])
+        f2= int(request.POST['f2'])
+        bins_num = int(request.POST['bins_num'])
+        g1 = generator.Generator(amplitude1, start_time1, duration1,
+                                 T1, kw1, jump_time1, p1, f1, fct1, bins_num*5)
+        g2 = generator.Generator(amplitude2, start_time2, duration2,
+                                 T2, kw2, jump_time2, p2, f2, fct2, bins_num * 5)
+        o = operator.Operator(g1, operation, g2)
+        plot = o.generate_plot()
+
+        mean_value = o.calculate_mean_value(g1.t1, g1.t1 + g1.d)
+        mean_abs_value = o.calculate_mean_abs_value(g1.t1, g1.t1 + g1.d)
+        effective_value = o.calculate_effective_value(g1.t1, g1.t1 + g1.d)
+        variance = o.calculate_variance(g1.t1, g1.t1 + g1.d)
+        mean_power = o.calculate_mean_power(g1.t1, g1.t1 + g1.d)
+        return render(request, "signal_and_noise_generation/plot.html",
+                      {'plot': plot,
+                       'mean_value': mean_value,
+                       'mean_abs_value': mean_abs_value,
+                       'effective_value': effective_value,
+                       'variance': variance,
+                       'mean_power': mean_power})
 
 def draw_plot(request):
     if request.method == 'POST':
